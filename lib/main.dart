9d46deecc300c71e118c01ef7eed76f89a4a3445
navid <navidrahman92@gmail.com>
2023-05-17 21:32:01 +0600

    Added 7 buttons to create 7 different tune.

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,75 @@ class XylophoneApp extends StatelessWidget {
 
   AudioPlayer player = AudioPlayer();
 
+  void playSound(int tuneNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$tuneNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: const Text('Click me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red)),
+                onPressed: () {
+                  playSound(1);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.orange)),
+                onPressed: () {
+                  playSound(2);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.yellow)),
+                onPressed: () {
+                  playSound(3);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green)),
+                onPressed: () {
+                  playSound(4);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.teal)),
+                onPressed: () {
+                  playSound(5);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                onPressed: () {
+                  playSound(6);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.purple)),
+                onPressed: () {
+                  playSound(7);
+                },
+                child: const Text(''),
+              ),
+            ],
           ),
         ),
       ),
